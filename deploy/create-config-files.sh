17df655c61405bfbd68e5f10323b4818267f0d4c
Talen Fisher <talen.fisher@cythral.com>
2020-09-09 18:57:02 -0500

    Add image param

--- deploy/create-config-files.sh
+++ deploy/create-config-files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cwd=$(dirname "${BASH_SOURCE[0]}")
+image=$1
 
 for file in $(ls ${cwd}/params/*.json); do
     envName=$(echo $file | xargs basename | sed "s/\.json//")
     params=$(cat $file)
+    params=$(echo $params | jq ".Image=\"$image\"")
     
     config={}
     config=$(echo $config | jq --argjson params "$params" '.Parameters=$params')
