2a4fcaa752a613469e35c8e5379e22d431798948
Talen Fisher <talen.fisher@cythral.com>
2020-11-24 12:25:29 -0600

    CYTHINF-195 Minor Client Fixes

--- src/Client/ServiceCollectionExtensions.cs
+++ src/Client/ServiceCollectionExtensions.cs
@@ -8,17 +8,18 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
-        public static void UseBrighidIdentity<TClass>(this IServiceCollection serviceCollection, string baseAddress)
+        public static void UseBrighidIdentity<TServiceType, TImplementation>(this IServiceCollection serviceCollection, string baseAddress)
             where TClass : class
         {
             serviceCollection.TryAddSingleton<TokenCache>();
             serviceCollection.TryAddScoped<IdentityServerClient>();
+            serviceCollection.TryAddScoped<ClientCredentialsHandler>();
 
             serviceCollection
             .AddHttpClient<IdentityServerClient>(options => options.BaseAddress = new Uri("https://identity.brigh.id/"));
-
+            
             serviceCollection
-            .AddHttpClient<TClass>(options => options.BaseAddress = new Uri(baseAddress))
+            .AddHttpClient<TServiceType, TImplementation>(options => options.BaseAddress = new Uri(baseAddress))
             .AddHttpMessageHandler<ClientCredentialsHandler>();
         }
     }
