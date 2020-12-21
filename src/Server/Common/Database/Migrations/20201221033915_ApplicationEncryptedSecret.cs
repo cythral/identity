bc3f3fbaf936e0a4a00019274410ea6d38109948
Talen Fisher <talen.fisher@cythral.com>
2020-12-20 21:47:35 -0600

    Add migration

---
+++ src/Server/Common/Database/Migrations/20201221033915_ApplicationEncryptedSecret.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Brighid.Identity.Migrations
+{
+    public partial class ApplicationEncryptedSecret : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedSecret",
+                table: "Applications",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedSecret",
+                table: "Applications");
+        }
+    }
+}
