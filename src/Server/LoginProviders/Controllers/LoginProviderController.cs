f05849b64f2ed45a114507758feb6eeea70e3a2b
Talen Fisher <talen.fisher@cythral.com>
2021-04-20 13:49:46 -0500

    Create Login Providers Controller

---
+++ src/Server/LoginProviders/Controllers/LoginProviderController.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+
+using Brighid.Identity.Roles;
+using Brighid.Identity.Users;
+
+using Microsoft.AspNetCore.Mvc;
+
+#pragma warning disable IDE0050
+
+namespace Brighid.Identity.LoginProviders
+{
+    [Route("/api/login-providers")]
+    [Roles(new[]
+    {
+        nameof(BuiltInRole.Basic),
+        nameof(BuiltInRole.Administrator),
+    })]
+    public class LoginProviderController : Controller
+    {
+        private readonly string[] UserEmbeds = new[] { "Roles.Role", "Logins" };
+
+        private readonly IUserRepository repository;
+
+        public LoginProviderController(
+            IUserRepository repository
+        )
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("{loginProvider}/{providerKey}")]
+        public async Task<ActionResult<User>> GetUserByLoginProvider(string loginProvider, string providerKey)
+        {
+            var result = await repository.FindByLogin(loginProvider, providerKey, UserEmbeds);
+            return result == null ? NotFound() : Ok(result);
+        }
+    }
+}
