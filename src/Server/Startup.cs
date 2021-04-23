3c051a90e52267c0cb993892ae857fd4ea570e6d
Talen Fisher <talen.fisher@cythral.com>
2021-04-23 10:54:52 -0500

    Split forwarded for address value by comma

--- src/Server/Startup.cs
+++ src/Server/Startup.cs
@@ -197,9 +197,9 @@ namespace Brighid.Identity
                 context.Request.Headers.TryGetValue("x-forwarded-for", out var forwardedForAddressValues);
                 var internalRange = IPNetwork.Parse(NetworkConfig.InternalIpv4Cidr);
                 var remoteAddress = context.Connection.RemoteIpAddress;
-
-                Console.WriteLine("Received forwarded for address: " + forwardedForAddressValues.FirstOrDefault());
-                var forwardedForAddress = forwardedForAddressValues.Count == 0 ? null : IPAddress.Parse(forwardedForAddressValues.First());
+                var forwardedForAddress = forwardedForAddressValues.Count == 0
+                    ? null
+                    : IPAddress.Parse(forwardedForAddressValues.First().Split(',').ElementAt(0));
 
                 if (!env.IsDevelopment() &&
                     (remoteAddress == null || !IPAddress.IsLoopback(remoteAddress)) &&
