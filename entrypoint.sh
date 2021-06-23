0272b41ff9f8e1849053c0def0f2db96d3bd3815
Talen Fisher <talen.fisher@cythral.com>
2021-06-22 21:17:02 -0500

    Tag Image on Startup + Add ECR Lifecycle Policy

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,18 @@ decrypt() {
     rm $tempfile;
 }
 
+tag_image() {
+    TAG=$1
+    IFS=':' read -ra IMAGE_PARTS <<< "$App__Image"
+    IFS='/' read -ra REPOSITORY_PARTS <<< "${IMAGE_PARTS[0]}"
+    REPOSITORY_NAME=${REPOSITORY_PARTS[1]}
+    TAG_NAME=${IMAGE_PARTS[1]}
+
+    MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY_NAME --image-ids imageTag=$TAG_NAME --query 'images[].imageManifest' --output text)
+    aws ecr put-image --repository-name $REPOSITORY_NAME --image-tag $TAG --image-manifest "$MANIFEST" >/dev/null || return 0
+}
+
 export Database__Password=$(decrypt ${Encrypted__Database__Password})
 
+tag_image $Environment__Name
 dotnet /app/Server.dll
