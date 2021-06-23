dea94b1fa363f092d744ed803e2e6478745ef60f
Talen Fisher <talen.fisher@cythral.com>
2021-06-22 22:23:19 -0500

    Specify Registry ID

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,17 @@ decrypt() {
 
 tag_image() {
     TAG=$1
+    
     IFS=':' read -ra IMAGE_PARTS <<< "$App__Image"
+    IFS='.' read -ra REGISTRY_PARTS <<< "${IMAGE_PARTS[0]}"
     IFS='/' read -ra REPOSITORY_PARTS <<< "${IMAGE_PARTS[0]}"
+    
+    REGISTRY_ID=${REGISTRY_PARTS[0]}
     REPOSITORY_NAME=${REPOSITORY_PARTS[1]}
     TAG_NAME=${IMAGE_PARTS[1]}
 
-    MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY_NAME --image-ids imageTag=$TAG_NAME --query 'images[].imageManifest' --output text)
-    aws ecr put-image --repository-name $REPOSITORY_NAME --image-tag $TAG --image-manifest "$MANIFEST" >/dev/null || return 0
+    MANIFEST=$(aws ecr batch-get-image --registry-id $REGISTRY_ID --repository-name $REPOSITORY_NAME --image-ids imageTag=$TAG_NAME --query 'images[].imageManifest' --output text)
+    aws ecr put-image --registry-id $REGISTRY_ID --repository-name $REPOSITORY_NAME --image-tag $TAG --image-manifest "$MANIFEST" >/dev/null || return 0
 }
 
 export Database__Password=$(decrypt ${Encrypted__Database__Password})
