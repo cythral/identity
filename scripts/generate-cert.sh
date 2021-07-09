94095d7771243840f116f34e7a896258bbe7129d
Talen Fisher <talen.fisher@cythral.com>
2021-07-09 10:59:39 -0500

    BRGD-145 Remove Generate Certificate Script

--- scripts/generate-cert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cwd=$(dirname ${BASH_SOURCE[0]})
-mkdir -p $cwd/.cert
-openssl req -x509 -days 90 -nodes -newkey rsa:4096 -keyout $cwd/.cert/rsa_private.pem -out $cwd/.cert/rsa_cert.pem -subj "/CN=cythral.com"
-openssl pkcs12 -export -in $cwd/.cert/rsa_cert.pem -inkey $cwd/.cert/rsa_private.pem -out $cwd/../certs/cert.pfx
