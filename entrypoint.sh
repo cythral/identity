909c7e17ec0fce0a6e9e237236beffaba9b31fbc
Talen Fisher <talen.fisher@cythral.com>
2022-02-09 11:00:48 -0600

    BRGD-200 Exit if Decryption Fails (#193)

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 if [ "$Environment" != "local" ]; then
-    export Database__Password=$(decrs ${Encrypted__Database__Password});
+    export Database__Password=$(decrs ${Encrypted__Database__Password}) || exit 1;
     runuser --user brighid /app/Server
     exit $?
 fi
